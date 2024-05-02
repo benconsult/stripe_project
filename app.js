b2586297f4d4b39fb5282e1c3b3a5c3ee567cfa1
benconsult <bendayo38@gmail.com>
2024-05-02 22:17:13 +0100

    setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // controller
-
+//check
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
